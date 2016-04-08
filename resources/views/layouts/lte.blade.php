5239f16802d56a368e383956e2a66bd85a7a416c
Afzal Hossain <afzal.csedu@gmail.com>
2016-04-08 17:06:23 +0600

    title change

--- resources/views/layouts/lte.blade.php
+++ resources/views/layouts/lte.blade.php
@@ -220,10 +220,6 @@ desired effect
                         </li>
                         @endcan
                     @endif
-
-                    @if(\Gate::allows('super'))
-                        <h2>super admin</h2>
-                        @endif
                 </ul>
         </section>
         <!-- /.sidebar -->
